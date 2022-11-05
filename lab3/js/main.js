5b833c9c3d91cf6f523da474478bbac8e799d5ee
Filip Helbig <helbig@filips-macbook-air.home>
2022-11-05 17:58:55 +0100

    Adding folder for lab3

---
+++ lab3/js/main.js
@@ -0,0 +1,7 @@
+const time12h = document.getElementById("time12h");
+time12h.addEventListener("input", time12hManagement);
+
+function time12hManagement() {
+  time12h.style = "background_color: black;";
+  console.log("done")
+}
