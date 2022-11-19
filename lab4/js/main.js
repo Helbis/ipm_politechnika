48162572005bbe0e78e6ba416f2debf0ac487caa
Filip Helbig <filippiotrhelbig@gmail.com>
2022-11-19 19:08:07 +0100

    some support for indexedDB

--- lab4/js/main.js
+++ lab4/js/main.js
@@ -3,3 +3,14 @@ function addUser(event){
 
   const formElements = document.getElementById("newUserData");
 }
+
+
+let request = window.indexedDB.open("newDatabase", 1);
+
+request.onupgradeneeded = () => {
+  alert("indexedDB update needed");
+}
+
+request.onsuccess = () => {
+  alert("indexedDB: DataBase loaded successfully!");
+}
