d1f4513fcad95e42c9f971feeeb76eef697c56a4
Filip Helbig <filippiotrhelbig@gmail.com>
2022-11-19 19:44:41 +0100

    refactoring js

--- lab4/js/main.js
+++ lab4/js/main.js
@@ -1,44 +1,70 @@
-function addUser(event){
-  event.preventDefault();
+window.indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB;
 
-  const formElements = document.getElementById("newUserData");
-  
-  const request = db.transaction(["client"], "readwrite")
-                    .objectStore("client")
-                    .add({
-    fname: document.getElementById("fname").value,
-    lname: document.getElementById("lname").value,
-    email: document.getElementById("email").value
-  });
+  window.IDBTransaction =
+    window.IDBTransaction ||
+    window.webkitIDBTransaction ||
+    window.msIDBTransaction;
+    window.IDBKeyRange =
+    window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+
+if (!window.indexedDB) {
+  window.alert("Your browser doesn't support a stable version of IndexedDB.");
 }
-
 
 let db;
 let request = window.indexedDB.open("newDatabase", 1);
 
-request.onupgradeneeded = () => {
-  const logMsg = "indexedDB update needed";
-  alert(logMsg);
-  console.log(logMsg);
-  
-  let db = event.target.result;
-  let objectStore = db.createObjectStore("client", { autoIncrement: true });
-  
+request.onerror = function (event) {
+  console.log("error: The database is opened failed");
+};
+
+request.onsuccess = function (event) {
+  db = request.result;
+  console.log("success: The database " + db + " is opened successfully");
+  drawTable();
+};
+
+request.onupgradeneeded = function (event) {
+  var db = event.target.result;
+  var objectStore = db.createObjectStore("client", { autoIncrement: true, });
+
   objectStore.createIndex("fname", "fname", { unique: false });
   objectStore.createIndex("lname", "lname", { unique: false });
-  objectStore.createIndex("email", "email", { unique: false });
-}
+  objectStore.createIndex("email", "email", { unique: true });
 
-request.onsuccess = () => {
-  const logMsg = "indexedDB: DataBase " + db + " loaded successfully!";
-  alert(logMsg);
-  console.log(logMsg);
-  
-  db = request.result;
-}
+  for (var i in clientData) {
+      objectStore.add(clientData[i]);
+    }
+};
 
-request.onerror = (e) => {
-  const errMsg = "indexedDB errro: database not loaded";
-  console.error(errMsg);
-  alert(errMsg);
+function add(event) {
+  event.preventDefault();
+
+  var formElements = document.getElementById("addForm");
+
+  var request = db
+    .transaction(["client"], "readwrite")
+    .objectStore("client")
+    .add({
+      name: formElements[0].value,
+      lastName: formElements[1].value,
+      age: formElements[2].value,
+      email: formElements[3].value,
+      pesel: formElements[4].value,
+      address: formElements[5].value,
+      phoneNumber: formElements[6].value,
+    });
+
+    request.onsuccess = function (event) {
+    console.log("Client added");
+    drawTable();
+};
+
+request.onerror = function (event) {
+    alert("Unable to add data\r\ user with that email aready exist in your database! ");
+  };
 }
