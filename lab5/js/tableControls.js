67d188e25a90757f5bd5afaf3d3a1942395a2944
Filip Helbig <filippiotrhelbig@gmail.com>
2022-12-10 19:09:49 +0100

    Table theme configuration

--- lab5/js/tableControls.js
+++ lab5/js/tableControls.js
@@ -1,7 +1,13 @@
-function markRow(){
-	document.getElementByTag("tr");
-}
+const trList = document.getElementsByTagName("tr");
 
 function update(){
 	// On table change, update the onclick event listener
+	for (const el of trList){
+		el.addEventListener("click", markRow);
+	}
+}
+
+function markRow(){
+	// mark row for edition
+
 }
